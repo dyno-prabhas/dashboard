817eacb8f6eef24482d489aabb8d0bf7630240b1
prabhas <prabhas.varma.tirumalaraju@gmail.com>
2025-01-24 19:45:15 +0530

    full project after remove customers page

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -19,7 +19,6 @@ const links = [
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
